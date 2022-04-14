da1f0fa405cd9cef1c9f09860ebb429f97e692d4
Amuche Ezeagu <amucheezeagu@gmail.com>
2022-04-14 00:05:19 +0000

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "main.h
+#include "main.h"
 /**
  * jack_bauer - prints every minute of the day of jack Bauer, starting from 00:00 to 23:59.
  */
@@ -20,4 +20,3 @@ void jack_bauer(void)
 		}
 	}
 }
-
