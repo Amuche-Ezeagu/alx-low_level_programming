55740056a2d6c2f54828b43f2127f4af16312176
Amuche Ezeagu <amucheezeagu@gmail.com>
2022-04-14 04:00:02 +0000

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,37 +7,48 @@
  */
 void times_table(void)
 {
+
 	int a = 0;
 	int b;
 	int c;
 	while (a <= 9)
+
 	{
 		b = 0;
 		while (b <= 9)
+
 		{
 			c = a * b;
 			if (b == 0)
+
 			{
 				_putchar(c + '0');
 			}
 			else if (c < 10)
+
 			{
 				_putchar(' ');
 				_putchar(c + '0');
 			}
 			else
+
 			{
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
 			}
-			if (b > 9)
+			if (b > 9
+					)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+
 			b++;
+
 		}
 		_putchar('\n');
+
 		a++;
+
 	}
 }
