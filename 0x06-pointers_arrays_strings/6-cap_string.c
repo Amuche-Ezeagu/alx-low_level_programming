0c8d03341f6e36536622a14901c70dfffd19ff53
Amuche Ezeagu <amucheezeagu@gmail.com>
2022-04-24 23:45:14 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *a)
 	char separator[] = " \t\n,;.!?\"(){}";
 	int i, j;
 
-	for (i, = 0; a[i] != '\0'; i++)
+	for (i = 0; a[i] != '\0'; i++)
 	{
 
 		if (a[i] >= 'a' && a[i] <= 'z')
