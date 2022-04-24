252a8fc0b64cb7ecf0531db409a1bf531696735a
Amuche Ezeagu <amucheezeagu@gmail.com>
2022-04-24 23:54:52 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @a: is a pointer type char
+ * Return: 0
+ *
+ */
+char *leet(char *a)
+{
+	char leet[] = "aAeEoOtTlL";
+	char chan[] = "4433007711";
+	int i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+
+		for (j = 0; leet[j] != '\0'; j++)
+				{
+					if (a[i] == leet[j])
+						a[i] = chan[i];
+				}
+
+	}
+	return (a);
+}
+
