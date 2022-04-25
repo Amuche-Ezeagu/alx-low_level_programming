a7f5288f2de6caac20b8ec0ebc063850f4b765ba
Amuche Ezeagu <amucheezeagu@gmail.com>
2022-04-25 03:20:50 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ char *leet(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; leet[j] != '\0'; j++)
+	for (j = 0; leet[j] != '\0'; j++)
 				{
 					if (a[i] == leet[j])
 						a[i] = chan[i];
